8b8a1d3c7e66300a658cb59f0170fc524125be89
gacbert <gacbert@users.noreply.github.com>
2025-06-14 07:36:40 -0400

    Include Gemini and DeepSeek deps

--- setup.py
+++ setup.py
@@ -15,6 +15,9 @@ setup(
     install_requires=[
         "langchain>=0.1.0",
         "langchain-openai>=0.0.2",
+        "langchain-google-genai",
+        "langchain-deepseek",
+        "deepseek",
         "langchain-experimental>=0.0.40",
         "langgraph>=0.0.20",
         "numpy>=1.24.0",
